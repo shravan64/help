d105ebc5dfdaf6df5136599bb1b8ac410bf990b4
shravan64 <45965997+shravan64@users.noreply.github.com>
2019-09-27 16:37:40 +0530

    Update config.js

--- VoiceBot-master/config.js
+++ VoiceBot-master/config.js
@@ -1,16 +1,16 @@
 var config = {
   // Watson Conversation Service Credentials
-  workspace_id          : "",
-  conversation_username : "",
-  conversation_password : "",
+  workspace_id          : "5bcf48a2-78f3-4680-bf3a-ba35a2ead32e",
+  conversation_username : "smhamal@tangentia.com",
+  conversation_password : "Shravan64#",
 
   // Watson Speech-To-Text Service Credentials
-  STT_username  : "",
-  STT_password  : "",
+  STT_username  : "smhamal@tangentia.com",
+  STT_password  : "Shravan64#",
 
   // Watson Text-To-Speech Service Credentials
-  TTS_username  : "",
-  TTS_password  : ""
+  TTS_username  : "smhamal@tangentia.com",
+  TTS_password  : "Shravan64#"
 }
 
 module.exports = config;
